5f679b0678ecd7d1cc256af10face68719144201
Nelson Earle <nelson.earle137@gmail.com>
2023-03-12 22:34:41 -0500

    add test doc links

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! Scratch project for testing things.
+//!
+//! ## Cross-crate doc links test
+//!
+//! - [Link to docs.rs `bevy::app::App`](https://docs.rs/bevy/*/bevy/app/struct.App.html)
+//! - [Link to crate dep `bevy_app::App`](bevy_app::App)
+//! - [Link to crate `bevy::app::App` (broken)](bevy::app::App)
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
